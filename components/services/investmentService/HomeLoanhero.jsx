fc31708b9b1960902d45fe40bc3ed05aa8faa6e8
DAppArtisan <dexterdigitaltechnologies@gmail.com>
2023-11-09 03:47:19 +0530

    change url

--- components/services/investmentService/HomeLoanhero.jsx
+++ components/services/investmentService/HomeLoanhero.jsx
@@ -4,41 +4,7 @@ import React from "react";
 
 const CallToAction = () => {
   return (
-    <div style={{background:'#f6f9fb'}} className="fancy-short-banner-twelve position-relative mt-90 zn2 pt-160 lg-pt-50">
-      <div className="container">
-        <div className="border-bottom pb-140 lg-pb-40">
-          <div className="row">
-            <div className="col-xxl-9 col-xl-10 m-auto text-center">
-              <div
-                className=" wow"
-                // data-aos="fade-up"
-                // data-aos-delay="100"
-              >
-                <h2 className="main-text fw-normal tx-dark">
-                Investment Loans Melbourne
-                </h2>
-              </div>
-              {/* /.title-style-ten */}
-              <p
-                className=" text-lg fs-24  mt-20 mb-35 lg-mb-30 lg-mt-20"
-                // data-aos="fade-up"
-                // data-aos-delay="200"
-              >
-               Building Your Financial Portfolio with Unify Finance Investment Loan Services in Melbourne
-
-
-              </p>
-              <p className="fs-18 mt-20 mb-35 lg-mb-30 lg-mt-20">
-              Ready to grow your wealth through strategic investments? Unify Finance's Investment Loan Services in Melbourne are tailored to help you seize profitable opportunities and build a diversified financial portfolio.
-              </p>
-            
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <div className="shapes shape-one" />
-      <div className="shapes shape-two" /> */}
-    </div>
+    <div></div>
   );
 };
 
